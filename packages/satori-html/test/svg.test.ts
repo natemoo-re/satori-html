ca646d2da56e729bfba2ac9c55c1e2c24417c3b6
Nate Moore <nate@astro.build>
2022-10-19 21:04:46 -0500

    test: improve tests

--- packages/satori-html/test/svg.test.ts
+++ packages/satori-html/test/svg.test.ts
@@ -29,4 +29,44 @@ describe("svg", () => {
     );
     expect(toImage(result, 100)).toMatchImageSnapshot();
   });
+
+  it("should handle style", async () => {
+    const result = await satori(
+      html`<div>Hello world</div><style>div { color: red; }</style>`,
+      {
+        width: 100,
+        height: 100,
+        fonts,
+      }
+    );
+    expect(toImage(result, 100)).toMatchImageSnapshot();
+  });
+
+  it("should handle tailwind", async () => {
+    const result = await satori(
+      html`<div class="bg-gray-50 flex">
+    <div class="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+      <h2 class="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+        <span>Ready to dive in?</span>
+        <span class="text-indigo-600">Start your free trial today.</span>
+      </h2>
+      <div class="mt-8 flex md:mt-0">
+        <div class="flex rounded-md shadow">
+          <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white">Get started</a>
+        </div>
+        <div class="ml-3 flex rounded-md shadow">
+          <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+`,
+      {
+        width: 800,
+        height: 150,
+        fonts,
+      }
+    );
+    expect(toImage(result, 100)).toMatchImageSnapshot();
+  });
 });
